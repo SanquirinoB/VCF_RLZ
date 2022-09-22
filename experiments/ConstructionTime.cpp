3d3d0dfd266a3000ffd411478b17235610a55a46
SanquirinoB <fernanda.sanchirico@ug.uchile.cl>
2022-09-22 16:29:38 -0300

    better testing process

--- experiments/ConstructionTime.cpp
+++ experiments/ConstructionTime.cpp
@@ -17,6 +17,8 @@ using namespace std;
 int main(int argc, char **argv)
 {
     string commands_file_path(argv[1]);
+    string skipParse(argv[3]);
+    string skipSort(argv[4]);
     ifstream commands_file(commands_file_path);
     string command_aux;
     vector<string> commands;
@@ -28,40 +30,68 @@ int main(int argc, char **argv)
     NanoTimer timer;
     double timeElapsed, size;
     string results_file_path(argv[2]);
-    ofstream results_file(results_file_path);
-    results_file << "ID\ttime\tfinalSize\n";
+    ofstream results_file(results_file_path, ios_base::app);
+    // results_file << "ID\tprocess\ttime\tfinalSize\n";
     results_file.flush();
 
     for (string command:commands)
     {
         string destPath = command.substr(0, command.find(" "));
-        cout << "Executing: " << endl;
-        cout << "\t" << ("python3 ./VCF_parsing/parsing_process.py " + command) << endl;
-        
-        timer.reset();
-        // Python parsing
-        if (system(("python3 ./VCF_parsing/parsing_process.py " + command).c_str()) != 0)
+        if(strcmp(skipParse.c_str(),"TRUE") != 0)
         {
-            cout << "[RLZ]\tParsing failed, check log for more information :(" << endl;
-            return -1;
+            cout << "Executing: " << endl;
+            cout << "\t" << ("python3 ./VCF_parsing/parsing_process.py " + command) << endl;
+            cout << " PARSE starting..." << endl;
+            timer.reset();
+            // Python parsing
+            if (system(("python3 ./VCF_parsing/parsing_process.py " + command).c_str()) != 0)
+            {
+                cout << "[RLZ]\tParsing failed, check log for more information :(" << endl;
+                return -1;
+            }
+            timeElapsed = timer.getMilisec();
+
+            results_file << destPath << "\t" << "PARSE" << "\t" << timeElapsed << "\t" << "?" << "\n";
+            results_file.flush();
+            cout << "   PARSE Ended: " << endl;
+            cout << destPath << "\t" << "PARSE" << "\t" << timeElapsed << "\t" << "?" << "\n";
+        }
+        else
+        {
+            cout << "   PARSE skipped..." << endl;
         }
 
-        VCFParsingSorter* Sorter = new VCFParsingSorter();
-        Sorter->StartProcess(destPath);
+        if(strcmp(skipSort.c_str(),"TRUE") != 0)
+        {
+            cout << " SORT starting..." << endl;
+            timer.reset();
+            VCFParsingSorter* Sorter = new VCFParsingSorter();
+            Sorter->StartProcess(destPath);
+            timeElapsed = timer.getMilisec();
+            results_file << destPath << "\t" << "SORT" << "\t" << timeElapsed << "\t" << "?" << "\n";
+            results_file.flush();
+            cout << "   SORT Ended: " << endl;
+            cout << destPath << "\t" << "SORT" << "\t" << timeElapsed << "\t" << "?" << "\n";
+            delete Sorter;
+        }
+        else
+        {
+            cout << "   SORT skipped..." << endl;
+        }
 
+        cout << " BUILD starting..." << endl;
+        timer.reset();
         VCFParsingInterpreter* Interpreter = new VCFParsingInterpreter();
         Interpreter->InitializeFromParsing(destPath);
 
         timeElapsed = timer.getMilisec();
         size = Interpreter->GetSize();
-        results_file << destPath << "\t" << timeElapsed << "\t" << size << "\n";
+        results_file << destPath << "\t" << "BUILD" << "\t" << timeElapsed << "\t" << size << "\n";
         results_file.flush();
-        cout << "   Ended: " << endl;
-        cout << destPath << "\t" << timeElapsed << "\t" << size << endl;
+        cout << "   BUILD Ended: " << endl;
+        cout << destPath << "\t" << "BUILD" << "\t" << timeElapsed << "\t" << size << "\n";
         Interpreter->SaveInterpreter();
     }
-
-    
 
     return 0;
 }
